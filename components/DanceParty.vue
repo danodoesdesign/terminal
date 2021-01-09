eef05e76619c3e863a708630b5dbba18b8727507
dano does design <dano.does.design@gmail.com>
2021-01-09 14:43:36 +1100

    csv parse works +added dance fun time

---
+++ components/DanceParty.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="gradient cursor-pointer" ref="party" @click="sendExitEmit">
+    <div
+      id="dance"
+      class="text-6xl flex flex-col items-center justify-center font-black min-h-screen"
+    >
+      <p class="text-center">STOP DANCING</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      audio: new Audio('tst.mp3'),
+    }
+  },
+  mounted() {
+    this.audio.play()
+    this.$refs.party.focus()
+  },
+  destroyed() {
+    this.audio.pause()
+  },
+  methods: {
+    sendExitEmit() {
+      this.$emit('exit')
+      console.log('exit emit sent')
+    },
+  },
+}
+</script>
+
+<style scoped>
+#dance {
+  font-family: Arial, Helvetica, sans-serif;
+  @apply text-white;
+}
+
+.gradient {
+  height: 100%;
+  width: 100%;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  position: absolute;
+  background: linear-gradient(
+    124deg,
+    #ff2400,
+    #e81d1d,
+    #e8b71d,
+    #e3e81d,
+    #1de840,
+    #1ddde8,
+    #2b1de8,
+    #dd00f3,
+    #dd00f3
+  );
+  background-size: 1800% 1800%;
+
+  -webkit-animation: rainbow 6s ease infinite;
+  -z-animation: rainbow 6s ease infinite;
+  -o-animation: rainbow 16s ease infinite;
+  animation: rainbow 6s ease infinite;
+}
+
+@-webkit-keyframes rainbow {
+  0% {
+    background-position: 0% 82%;
+  }
+  50% {
+    background-position: 100% 19%;
+  }
+  100% {
+    background-position: 0% 82%;
+  }
+}
+@-moz-keyframes rainbow {
+  0% {
+    background-position: 0% 82%;
+  }
+  50% {
+    background-position: 100% 19%;
+  }
+  100% {
+    background-position: 0% 82%;
+  }
+}
+@-o-keyframes rainbow {
+  0% {
+    background-position: 0% 82%;
+  }
+  50% {
+    background-position: 100% 19%;
+  }
+  100% {
+    background-position: 0% 82%;
+  }
+}
+@keyframes rainbow {
+  0% {
+    background-position: 0% 82%;
+  }
+  50% {
+    background-position: 100% 19%;
+  }
+  100% {
+    background-position: 0% 82%;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <div
-    id="terminal"
-    ref="canvas"
-    class="block"
-    @click.native="focusCommandLine"
-  >
-    <div class="flex flex-row text-gray-600">
-      <pre class="text-2xs mb-10">
+  <div>
+    <div
+      v-if="terminalView"
+      id="terminal"
+      ref="canvas"
+      class="block"
+      @click.native="focusCommandLine"
+    >
+      <div class="flex flex-row text-gray-600">
+        <pre class="text-2xs mb-10">
                   (@@@@@@@@@@@@@                  
              @@@@@@@@   @@@   &@@@@@@@            
           @@@@@@@@      @@@       @@@@@@@         
@@ -27,48 +29,95 @@
              @@@@@@@    @@@    @@@@@@@            
                   @@@@@@@@@@@@@@@
                   </pre
-      >
-      <p id="introText" class="self-center hidden sm:inline-block ml-10">
-        danodoesdesign terminal<br />
-        version 0.2 pre-release<br />
-        next up: airtable API to parse and respond to commands
-      </p>
+        >
+        <p id="introText" class="self-center hidden sm:inline-block ml-10">
+          danodoesdesign terminal<br />
+          version 0.2 pre-release<br />
+          next up: airtable API to parse and respond to commands
+        </p>
+      </div>
+      <ResponseLine>we have wares, if you have the coin</ResponseLine>
+      <CommandLine id="the-line" v-on:command="progressTerminal" />
     </div>
-    <ResponseLine>we have wares, if you have the coin</ResponseLine>
-    <CommandLine id="the-line" v-on:command="progressTerminal" />
+    <div v-else>
+      <DanceParty v-on:exit="setTerminalView" />
+    </div>
   </div>
 </template>
 
 <script>
 import ResponseLine from '../components/ResponseLine.vue'
 import Vue from 'vue'
+import VuePapaParse from 'vue-papa-parse'
+Vue.use(VuePapaParse)
+
 export default {
+  data: function () {
+    return {
+      terminalView: true,
+      commandsList: [],
+    }
+  },
+  mounted() {
+    this.readCSV()
+  },
+
   methods: {
+    readCSV() {
+      // init Papa Parse with defined commands.csv sitting in static/
+      var file = 'commands.csv'
+      this.$papa.parse(file, {
+        // https://www.papaparse.com/docs -> parse remote file
+        download: true,
+        complete: this.onReadCSVComplete, // run seperate method (weird hack that made this work)
+      })
+    },
+
+    onReadCSVComplete(results, file) {
+      this.commandsList = results.data // set the Papa Parse returned data to the exisiting variable
+    },
+
     focusCommandLine() {
       document.getElementById('command-line').focus()
+    },
+
+    setTerminalView() {
+      console.log('terminal view called')
+      this.terminalView = true
     },
 
     runCommand(command) {
       console.log('runCommand called with ' + command)
 
-      /*
-      some sort of string check to see if command matches anything
-      it will need to retrieve:
-      - what to respond with
-      - type of response (error, success, future ideas like 'loading bar')
-      */
+      //define variables to be used below
+      var responseType = '' // can be success / error / empty(default)
+      var responseContent = ''
 
-      var responseType = 'success' // success / error / empty(default)
-      var responseContent = 'word(s) entered successfully'
+      var commandsLength = this.commandsList.length
+      for (var i = 0; i < commandsLength; i++) {
+        // loop through the parse results
+        if (this.commandsList[i][0] == command) {
+          // when the entered command matches the first column parse result
+          responseType = 'success'
+          responseContent = this.commandsList[i][1] // fill the variables from before, we actually do stuff with them below
+          break
+        } else if (command == 'dance') {
+          responseContent = 'stop dancing'
+          this.terminalView = false
+        } else {
+          responseType = 'error'
+          responseContent = 'no such command'
+        }
+      }
 
-      var ResponseComponentClass = Vue.extend(ResponseLine)
+      var ResponseComponentClass = Vue.extend(ResponseLine) // create uninitiated instance of ResponseLine component
 
       var ResponseInstance = new ResponseComponentClass({
-        propsData: { type: responseType },
+        propsData: { type: responseType }, // initiate instance of it passing in the responseType prop, we defined this above
       })
-      ResponseInstance.$slots.default = [responseContent]
-      ResponseInstance.$mount()
-      this.$refs.canvas.appendChild(ResponseInstance.$el)
+      ResponseInstance.$slots.default = [responseContent] // put the responseContent into the component's slot
+      ResponseInstance.$mount() // mount this instance
+      this.$refs.canvas.appendChild(ResponseInstance.$el) // append to the end of the div with ref of 'canvas'
     },
 
     progressTerminal(command) {
